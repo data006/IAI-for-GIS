a6d7d507ab14b909977517b77f5068144ba8ba03
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 12:18:25 -0600

    Add files via upload

---
+++ IAI for GIS/IAI for GIS/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace IAI_for_GIS
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(93, 62);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(268, 53);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Hola";
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(382, 62);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(268, 53);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.Text = "Adios";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(99, 204);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(10, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(259, 253);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(10, 10);
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(12, 338);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(797, 53);
+			this.textBox4.TabIndex = 6;
+			this.textBox4.Text = "Hola";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 20;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Interval = 75;
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(1350, 729);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Activated += new System.EventHandler(this.Form1_Activated);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+	}
+}
+
---
+++ IAI for GIS/IAI for GIS/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IAI_for_GIS
+{
+	public partial class Form1 : Form
+	{
+		int jaja = 0;
+		Color jeje = Color.FromArgb(255, 0, 255);
+
+		private readonly Random _random = new Random();
+
+		public int RandomNumber(int min, int max)
+		{
+			return _random.Next(min, max);
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		public void Form1_Load(object sender, EventArgs e)
+		{
+			Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+			Graphics graphics = Graphics.FromImage(bitmap as Image);
+			graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+			Cursor.Hide();
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			
+			textBox1.Text = "X: " + Convert.ToString(e.X);
+			textBox3.Text = "Y: " + Convert.ToString(e.Y);
+
+			//pictureBox1.Left = e.X - 3;
+			//pictureBox1.Top = e.Y - 2;
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			
+			//textBox1.Text = "X: " + Convert.ToString(e.X);
+			//textBox3.Text = "Y: " + Convert.ToString(e.Y);
+
+			//pictureBox1.Left = e.X - 5;
+			//pictureBox1.Top = e.Y - 5;
+			
+		}
+
+		private void Form1_Activated(object sender, EventArgs e)
+		{
+
+			
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			jaja = jaja + 1;
+			textBox4.Text = Convert.ToString(pictureBox1.BackColor);
+
+			if(jaja == 1)
+			{
+				int uno = RandomNumber(0, 255);
+				int dos = RandomNumber(0, 255);
+				int tres = RandomNumber(0, 255);
+				jeje = Color.FromArgb(uno, dos, tres);
+				pictureBox1.BackColor = jeje;
+			}
+			else
+			{
+				int actualUno = pictureBox1.BackColor.R;
+				int actualDos = pictureBox1.BackColor.G;
+				int actualTres = pictureBox1.BackColor.B;
+				int uno = RandomNumber(actualUno - 20, actualUno + 20);
+				int dos = RandomNumber(actualDos - 20, actualDos + 20);
+				int tres = RandomNumber(actualTres - 20, actualTres + 20);
+
+				if(uno > 255)
+				{
+					uno = 255;
+				}
+				if (uno < 200)
+				{
+					uno = 200;
+				}
+
+				if (dos > 255)
+				{
+					dos = 255;
+				}
+				if (dos < 200)
+				{
+					dos = 200;
+				}
+
+				if (tres > 255)
+				{
+					tres = 255;
+				}
+				if (tres < 200)
+				{
+					tres = 200;
+				}
+
+				jeje = Color.FromArgb(uno, dos, tres);
+				pictureBox1.BackColor = jeje;
+			}
+
+			
+
+			
+
+		}
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{	
+			if(pictureBox1.Visible == true)
+			{
+				pictureBox1.Visible = false;
+			}
+			else
+			{
+				pictureBox1.Visible = true;
+			}
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left)
+			{
+				pictureBox1.Left = pictureBox1.Left - 100;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				pictureBox1.Left = pictureBox1.Left + 100;
+			}
+
+			if (e.KeyCode == Keys.Up)
+			{
+				pictureBox1.Top = pictureBox1.Top + 100;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				pictureBox1.Top = pictureBox1.Top - 100;
+			}
+		}
+
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left)
+			{
+				pictureBox1.Left = pictureBox1.Left - 5;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				pictureBox1.Left = pictureBox1.Left + 5;
+			}
+
+			if (e.KeyCode == Keys.Up)
+			{
+				pictureBox1.Top = pictureBox1.Top - 5;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				pictureBox1.Top = pictureBox1.Top + 5;
+			}
+		}
+
+		private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			
+		}
+
+	}
+}
